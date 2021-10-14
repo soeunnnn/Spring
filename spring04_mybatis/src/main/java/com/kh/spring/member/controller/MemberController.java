5928abc82be4b34ba4abf78a33e7c6f3dbea103d
soeun <thdms12333@gmail.com>
2021-10-15 00:18:45 +0900

    커밋 테스트

--- spring04_mybatis/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ spring04_mybatis/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -12,6 +12,9 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
+	//커밋잘되는건가..???????????????????
+	
+	
 	@GetMapping("search-pw") //search-pw로 요청이 왔을 때 이 메서드가 실행되도록
 	public String searchPassword() {
 		System.out.println("비밀번호는 " + memberService.selectPasswordByUserId());
