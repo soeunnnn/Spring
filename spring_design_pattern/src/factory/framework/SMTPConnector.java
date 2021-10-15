fb4b3ce912e20cd8ed812e984522d13ecaa7ac3f
soeunnnn <90734058+soeunnnn@users.noreply.github.com>
2021-10-15 17:21:12 +0900

    Add files via upload

---
+++ spring_design_pattern/src/factory/framework/SMTPConnector.java
@@ -0,0 +1,35 @@
+package factory.framework;
+
+import java.util.Date;
+
+public abstract class SMTPConnector {
+	
+	protected String url;
+	protected String id;
+	protected String password;
+	protected Date ConnectTime;
+	
+	
+	public SMTPConnector(String url, String id, String password, Date connectTime) {
+		super();
+		this.url = url;
+		this.id = id;
+		this.password = password;
+		ConnectTime = connectTime;
+	}
+	
+	public abstract void connect();
+
+	@Override
+	public String toString() {
+		return "SMTPConnector [url=" + url + ", id=" + id + ", password=" + password + ", ConnectTime=" + ConnectTime
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ spring_design_pattern/src/proxy/framework/Developer.java
@@ -0,0 +1,8 @@
+package proxy.framework;
+
+//Man과 Woman을 같은 타입으로 다룰 수 있는 인터페이스를 하나 생성한 후 상속시킴
+public interface Developer {
+	
+	void develop();
+
+}
